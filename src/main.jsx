663b7e0e0512e5e6b5d5ff7dcee2a6448e262b77
venkatvijay-25 <venkatvijay-25@users.noreply.github.com>
2026-04-27 21:00:49 +0530

    Bundle Inter font

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-600.css";
+import "@fontsource/inter/latin-700.css";
+import "@fontsource/inter/latin-800.css";
 import App from "./App.jsx";
 import "./styles.css";
 
